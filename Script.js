8d3fe063457193bad10074623a6ada3bc4588f45
adasqaz <adamsxxx100@gmail.com>
2025-12-02 16:25:55 +0000

    TallScreenScaling

--- Script.js
+++ Script.js
@@ -1,6 +1,7 @@
 const Main = document.getElementById("Main");
 const Header = document.getElementById("Header");
 const Html = document.querySelector("html");
+const Head = document.querySelector("head");
 
 // let ImageDisplay = null;
 function VideoClicked(VideoName){
@@ -42,15 +43,22 @@ document.addEventListener("DOMContentLoaded", ()=>{
 	Html.appendChild(Measure);
 
 	const Rect = Measure.getBoundingClientRect();
+
+	const Style = document.createElement("link");
+	Style.rel = "stylesheet";
+
 	if (Rect.height > Rect.width) {
 		console.log("User Uses Tall Screen");
 		const Lights = document.getElementsByClassName("Lights");
 		Array.from(Lights).forEach(element => {
 			element.remove();
 		});
+		
+		Style.href = "./Styles/TallScreen.css";
 	}else{
 		console.log("User Uses Wide Screen");
+		Style.href = "./Styles/WideScreen.css";
 	}
-
+	Head.appendChild(Style);
 	Measure.remove();
 });
